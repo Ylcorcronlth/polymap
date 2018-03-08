eac07769ec2228b8ab3590bc74e43aefb736c4aa
Nicholas Jones <rho1.cancri@yahoo.com>
2018-03-08 15:50:40 -0500

    Added a test scene for IDW interpolation and tweaked the algorithm some.

--- Assets/Scripts/Interpolate/InverseDistanceWeighting.cs
+++ Assets/Scripts/Interpolate/InverseDistanceWeighting.cs
@@ -7,12 +7,14 @@ public class InverseDistanceWeighting {
     private float SearchRadius;  // The maximum distance at which points influence interpolation value.
     private List<int>[,] Chunks;  // The indices of points falling into cells in a 2Rx2R grid, for fast lookup.
     private int Size;  // The size of the above grid.
-    private float Exponent; // The exponent for distance weighting.
+    private float Exponent;  // The exponent for distance weighting.
+    private float FalloffWeight;  // The base weight used to prevent division by zero and make interpolation go to zero far from any points.
 
-    public InverseDistanceWeighting(Vector2[] points, float search_radius=0.05f, float exponent=2.0f) {
+    public InverseDistanceWeighting(Vector2[] points, float search_radius=0.05f, float exponent=2.0f, float falloff_weight=1.0f) {
         Points = points;
         SearchRadius = search_radius;
         Exponent = exponent;
+        FalloffWeight = falloff_weight;
         // Points are assumed to fall into the rectangle (0, 0, 1, 1).
         Size = (int)(1.0f/(2.0f*SearchRadius));
 
@@ -27,9 +29,11 @@ public class InverseDistanceWeighting {
         // Sort points into the grid.
         for (int i = 0; i < Points.Length; i++) {
             Vector2 pos = Points[i];
-            int x = Utils.Fastfloor(pos.x * Size);
-            int y = Utils.Fastfloor(pos.y * Size);
-            Chunks[x, y].Add(i);
+            if (pos.x >= 0.0f && pos.x <= 1.0f && pos.y >= 0.0f && pos.y <= 1.0f) {
+                int x = Utils.Fastfloor(pos.x * Size);
+                int y = Utils.Fastfloor(pos.y * Size);
+                Chunks[x, y].Add(i);
+            }
         }
     }
 
@@ -53,8 +57,8 @@ public class InverseDistanceWeighting {
         int xi = Utils.Fastfloor(pos.x*Size);
         int yi = Utils.Fastfloor(pos.y*Size);
 
-        int xo = (pos.x - xi > 0.5f ? 1 : -1);
-        int yo = (pos.y - yi > 0.5f ? 1 : -1);
+        int xo = (pos.x*Size - xi > 0.5f ? 1 : -1);
+        int yo = (pos.y*Size - yi > 0.5f ? 1 : -1);
 
         nearby_points.AddRange(GetChunk(xi, yi));
         nearby_points.AddRange(GetChunk(xi + xo, yi));
@@ -65,16 +69,17 @@ public class InverseDistanceWeighting {
     }
 
     public float Evaluate(Vector2 pos, float[] values, Vector2[] gradients) {
-        List<int> nearby_points = GetNearbyPoints(pos);
         // Leave early if we're too far outside the unit square.
         if (pos.x < -SearchRadius || pos.x > 1.0f + SearchRadius || pos.y < -SearchRadius || pos.y > 1.0f + SearchRadius) {
             return 0.0f;
         }
 
+        List<int> nearby_points = GetNearbyPoints(pos);
+
         // Compute weights for each point.
         List<float> weights = new List<float>();
         List<int> indices = new List<int>();
-        float weight_sum = 0.0f;
+        float weight_sum = FalloffWeight;
         foreach (int index in nearby_points) {
             Vector2 point = Points[index];
             float distance = (pos - point).magnitude;
---
+++ Assets/Scripts/Test/TextureCreator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCreator : MonoBehaviour {
+
+	public int Resolution = 64;
+	Texture2D Texture;
+	public float SearchRadius = 0.05f;
+	public float Exponent = 2.0f;
+	public float FalloffWeight = 1.0f;
+
+	private InverseDistanceWeighting Interpolator;
+	public Vector2[] Points;
+	public float[] Values;
+	public Vector2[] Gradients;
+
+	private void CreateInterpolator() {
+		Interpolator = new InverseDistanceWeighting(Points, SearchRadius, Exponent, FalloffWeight);
+	}
+
+	private void CreateTexture() {
+		Texture = new Texture2D(Resolution, Resolution, TextureFormat.RGB24, true);
+		Texture.name = "Procedural texture";
+		Texture.wrapMode = TextureWrapMode.Clamp;
+		GetComponent<MeshRenderer>().material.mainTexture = Texture;
+	}
+
+	private void FillTexture() {
+		for (int x = 0; x < Resolution; x++) {
+			for (int y = 0; y < Resolution; y++) {
+				float value = Interpolator.Evaluate(new Vector2((float)x/(Resolution - 1.0f), (float)y/(Resolution - 1.0f)), Values, Gradients);
+				//float value = 4.0f*(x/(Resolution - 1.0f) - 0.5f)*(x/(Resolution - 1.0f) - 0.5f);
+				//Debug.Log(x + ", " + y + ": " + value);
+				if (value > 1.0f) {
+					value = 1.0f;
+				} else if (value < -1.0f) {
+					value = -1.0f;
+				}
+				if (value > 0.0f) {
+					Texture.SetPixel(x, y, new Color(value, value, value));
+				} else {
+					Texture.SetPixel(x, y, new Color(-value, 0.0f, 0.0f));
+				}
+			}
+		}
+		Texture.Apply();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		CreateInterpolator();
+		CreateTexture();
+		FillTexture();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
